2f8b33f6282ca9a1d09beacb6ed942e0264ad530
João M. Bezerra <marcospb19@hotmail.com>
2022-11-01 22:30:05 -0300

    start main.rs and lib.rs
    binary executes nothing

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+use std::io::{stderr, stdout, Write};
 
+#[allow(dead_code)]
+struct Environment<W1, W2>
+where
+    W1: Write,
+    W2: Write,
+{
+    stdout: W1,
+    stderr: W2,
+}
+
+pub fn run_sushi(_input: &str) -> Result<(), ()> {
+    let (stdout, stderr) = (stdout(), stderr());
+    let _environment = Environment { stdout, stderr };
+
+    return Ok(());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use std::io::{stdin, Read};
+
+use sushi::run_sushi;
+
+fn main() {
+    let input = {
+        let mut string = String::new();
+        let _bytes = stdin().read_to_string(&mut string).unwrap();
+        string
+    };
+
+    run_sushi(&input).unwrap();
+}
