1506644f939b18b6993e882e303442f7bed7ed72
João M. Bezerra <marcospb19@hotmail.com>
2022-11-02 13:09:41 -0300

    implement lexer Iterator glue for LALRPOP

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,37 @@
-#[derive(logos::Logos, Debug, Clone, Copy)]
-enum Token<'input> {
+use logos::Logos;
+
+pub type LexerResult<'a> = std::result::Result<TokenWithSpan<'a>, LexerError>;
+pub type TokenWithSpan<'a> = (usize, Token<'a>, usize);
+pub type LexerError = ();
+
+pub struct Lexer<'a> {
+    lex: logos::SpannedIter<'a, Token<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Lexer {
+            lex: Token::lexer(src).spanned(),
+        }
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = LexerResult<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (token, span) = self.lex.next()?;
+
+        if matches!(token, Token::Invalid) {
+            todo!();
+        } else {
+            Some(Ok((span.start, token, span.end)))
+        }
+    }
+}
+
+#[derive(Logos, Debug, Clone, Copy)]
+pub enum Token<'input> {
     #[regex(r"[_a-zA-Z][a-zA-Z0-9_]*")]
     Ident(&'input str),
     #[regex(r"[0-9][0-9_]*")]
